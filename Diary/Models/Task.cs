eb428bdbed5857bd6c37f45f41b5d7ced8019a00
Olga Kul <helgaukrop@gmail.com>
2019-04-16 23:16:19 +0300

    Add class Task

---
+++ Diary/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diary.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public  string Priority { get; set; }
+
+        DateTime Start_date { get; set; }
+
+        DateTime End_date { get; set; }
+
+        public bool? Status { get; set; }
+
+    }
+}
